1651a2833e46ca2276662312c7a49d9112a985b9
henkieee <henkiexxx4@gmail.com>
2016-05-10 21:10:30 +0200

    Testing angular2 beta

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'test',
+    template: '<h1>Tester de testt!!!!</h1>'
+})
+
+export class testComponent{ }
---
+++ app/main.ts
@@ -0,0 +1,4 @@
+import {bootstrap} from 'angular2/platform/browser';
+import {testComponent} from './app.component';
+
+bootstrap(testComponent);
